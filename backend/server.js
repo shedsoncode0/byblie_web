37489ea5e8eddccb339b930ebbde6669d1887329
shedrackAI <aigbeshedrack0@gmail.com>
2023-06-23 05:24:19 -0900

    creating the backend route for conversation between users

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const color = require('colors');
-const morgan = require('morgan');
-require('dotenv').config();
+/** @format */
 
-const connectDB = require('./server/database/connection');
-const { verifyAccessToken } = require('./utils/jwt');
+const express = require("express");
+const cors = require("cors");
+const color = require("colors");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const connectDB = require("./server/database/connection");
+const { verifyAccessToken } = require("./utils/jwt");
 
 // Server PORT
 const PORT = process.env.PORT || 8000;
@@ -20,9 +22,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', verifyAccessToken, async (req, res) => {
+app.get("/", verifyAccessToken, async (req, res) => {
   res.status(200).json({
     id: req.user.id,
     fullname: req.user.fullname,
@@ -31,11 +33,13 @@ app.get('/', verifyAccessToken, async (req, res) => {
 });
 
 // Route Middlewares
-app.use('/api/v1/auth', require('./server/routes/authRoute'));
-app.use('/api/v1/user', require('./server/routes/userRoute'));
-app.use('/api/v1/post', require('./server/routes/post_route'));
+app.use("/api/v1/auth", require("./server/routes/authRoute"));
+app.use("/api/v1/user", require("./server/routes/userRoute"));
+app.use("/api/v1/post", require("./server/routes/post_route"));
+app.use("/api/v1/conversation", require("./server/routes/conversation_route"));
+app.use("/api/v1/messages", require("./server/routes/messages_route"));
 
 // Start the server
 app.listen(PORT, () => {
-  console.log('Server is up an Running on POST ' + PORT.rainbow.underline);
+  console.log("Server is up an Running on POST " + PORT.rainbow.underline);
 });
--- backend/server/routes/messages_route.js
+++ backend/server/routes/messages_route.js
@@ -1,7 +1,7 @@
 /** @format */
 
 const router = require("express").Router();
-const Conversation = require("../models/conversation_model");
+const Message = require("../models/message_model");
 
 router.post("/", async (req, res) => {
   const newConversation = new Conversation({
@@ -21,25 +21,6 @@ router.post("/", async (req, res) => {
       error,
     });
   } 
-});
-
-router.get("/:userId", async (req, res) => {
-  const userId = req.params.userId;
-  try {
-    const conversation = await Conversation.find({
-      members: { $in: [userId] },
-    });
-    res.status(200).json({
-      status: true,
-      message: "all conversation",
-      data: conversation,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: false,
-      error,
-    });
-  }
 });
 
 module.exports = router;
