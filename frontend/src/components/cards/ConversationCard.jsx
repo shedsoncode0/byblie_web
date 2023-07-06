cc99b575d07dc1509e023a12d048a1aea7a54f19
shedrack <aigbeshedrack0@gmail.com>
2023-07-06 19:03:38 +0100

    Update ConversationCard.jsx

--- frontend/src/components/cards/ConversationCard.jsx
+++ frontend/src/components/cards/ConversationCard.jsx
@@ -36,7 +36,7 @@ const ConversationCard = ({ conversation, currentUser, connectedUsers }) => {
   }, [connectedUsers, id]);
 
   return (
-    <div className="w-full md:hover:bg-slate-100 md:p-3 p-1 rounded-lg flex">
+    <div className="w-full relative md:hover:bg-slate-100 md:p-3 p-1 rounded-lg flex">
       <div className="max-w-[60px] relative w-full">
         <img
           className="rounded-full w-full h-full object-cover"
@@ -51,9 +51,12 @@ const ConversationCard = ({ conversation, currentUser, connectedUsers }) => {
           class={`absolute bottom-0 right-0 block h-3 w-3 rounded-full ring-2 ring-white bg-green-400`}
         ></div>
       </div>
+      <div
+          class={`absolute bottom-0 right-0 block h-3 w-3 rounded-full ring-2 ring-white bg-green-400`}
+        ></div>
       <div className="ml-3 hidden md:flex flex-1 items-start justify-center flex-col">
         <h1>{user.fullname}</h1>
-        <p className="text-gray-400">hello man</p>
+        <p className="text-gray-400">hello mansss</p>
       </div>
       <div className=" hidden md:flex items-center">
         <h1 className="text-sm">10:10pm</h1>
